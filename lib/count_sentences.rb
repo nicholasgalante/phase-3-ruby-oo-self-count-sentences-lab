3b1ef9d44dc982287dc64d733d79ca2e6f1d000e
Nicholas Galante <nicholasgalante94@gmail.com>
2023-02-20 23:51:59 -0500

    commit tree 40faba05a45485e12d09b86d94abcf2507c59536

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[?.!](?=\s)/).count
 
   end
+
 end
+
+"This is a test! Are you counting? Yes. No?".count_sentences
